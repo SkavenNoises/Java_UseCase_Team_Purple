c4a9f487aec3ad0d08f6753d03400638b9987908
Sinduri Guntupalli <sinduri.g@gmail.com>
2020-07-28 11:27:35 +0200

    Animal & Resource classes - Sinduri

---
+++ src/classes/Animal.java
@@ -0,0 +1,123 @@
+package classes;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class Animal {
+
+    private int animalID;
+    private static int animalStaticID = 0;
+    private String animalName;
+    private boolean isHealthy;
+    private Resource food;
+    private double quantityOfFoodEats;
+    private Resource medication;
+    private int quantityOfMedsRequired;
+
+    //Log of meds given, time and quantity
+    private HashMap<LocalDateTime, HashMap<Resource, Integer>> medsGivenAt = new HashMap<>();
+
+    //Log of food given, time and quantity
+    private HashMap<LocalDateTime, HashMap<Resource, Integer>> foodGivenAt = new HashMap<>();
+
+    public enum AnimalSpecies {
+        Tiger, Elephant, Panda, Bear, Monkey, Others
+    }
+
+    private AnimalSpecies animalSpecies;
+
+    static HashMap<Integer, Animal> animalsHash = new HashMap<>();
+
+    public Animal(String animalName, boolean isHealthy, Resource food, double quantityOfFoodEats, Resource medication, int quantityOfMedsRequired, classes.Animal.AnimalSpecies animalSpecies) {
+        animalStaticID++;
+        this.animalID = animalStaticID;
+        this.animalName = animalName;
+        this.isHealthy = isHealthy;
+        this.food = food;
+        this.quantityOfFoodEats = quantityOfFoodEats;
+        this.medication = medication;
+        this.quantityOfMedsRequired = quantityOfMedsRequired;
+        this.animalSpecies = animalSpecies;
+        animalsHash.put(this.animalID, this);
+    }
+
+    public int getAnimalID() {
+        return animalID;
+    }
+
+    public void setAnimalID(int animalID) {
+        this.animalID = animalID;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public boolean isHealthy() {
+        return isHealthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        isHealthy = healthy;
+    }
+
+    public Resource getFood() {
+        return food;
+    }
+
+    public void setFood(Resource food) {
+        this.food = food;
+    }
+
+    public double getQuantityOfFoodEats() {
+        return quantityOfFoodEats;
+    }
+
+    public void setQuantityOfFoodEats(double quantityOfFoodEats) {
+        this.quantityOfFoodEats = quantityOfFoodEats;
+    }
+
+    public Resource getMedication() {
+        return medication;
+    }
+
+    public void setMedication(Resource medication) {
+        this.medication = medication;
+    }
+
+    public int getQuantityOfMedsRequired() {
+        return quantityOfMedsRequired;
+    }
+
+    public void setQuantityOfMedsRequired(int quantityOfMedsRequired) {
+        this.quantityOfMedsRequired = quantityOfMedsRequired;
+    }
+
+    public HashMap<LocalDateTime, HashMap<Resource, Integer>> getMedsGivenAt() {
+        return medsGivenAt;
+    }
+
+    public void setMedsGivenAt(HashMap<LocalDateTime, HashMap<Resource, Integer>> medsGivenAt) {
+        this.medsGivenAt = medsGivenAt;
+    }
+
+    public HashMap<LocalDateTime, HashMap<Resource, Integer>> getFoodGivenAt() {
+        return foodGivenAt;
+    }
+
+    public void setFoodGivenAt(HashMap<LocalDateTime, HashMap<Resource, Integer>> foodGivenAt) {
+        this.foodGivenAt = foodGivenAt;
+    }
+
+    public AnimalSpecies getAnimalSpecies() {
+        return animalSpecies;
+    }
+
+    public void setAnimalSpecies(AnimalSpecies animalSpecies) {
+        this.animalSpecies = animalSpecies;
+    }
+}
---
+++ src/classes/Resource.java
@@ -0,0 +1,80 @@
+package classes;
+
+import java.util.HashMap;
+
+public class Resource {
+    private int resourceID;
+    private static int resourceStaticID = 0;
+    private String resourceName;
+    private double resourcePrice;
+    private String companyName;
+    private int quantityResource;
+
+    public enum ResourceType {
+        Food, Meds, Other
+    }
+
+    private ResourceType resourceType;
+
+    static HashMap<Integer, Resource> resourcesHash = new HashMap<>();
+
+    public Resource(String resourceName, double resourcePrice, String companyName, int quantityResource, classes.Resource.ResourceType resourceType) {
+        resourceStaticID++;
+        this.resourceID = resourceStaticID;
+        this.resourceName = resourceName;
+        this.resourcePrice = resourcePrice;
+        this.companyName = companyName;
+        this.quantityResource = quantityResource;
+        this.resourceType = resourceType;
+        resourcesHash.put(this.resourceID, this);
+    }
+
+    public int getResourceID() {
+        return resourceID;
+    }
+
+    public void setResourceID(int resourceID) {
+        this.resourceID = resourceID;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public double getResourcePrice() {
+        return resourcePrice;
+    }
+
+    public void setResourcePrice(double resourcePrice) {
+        this.resourcePrice = resourcePrice;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getQuantityResource() {
+        return quantityResource;
+    }
+
+    public void setQuantityResource(int quantityResource) {
+        this.quantityResource = quantityResource;
+    }
+
+    public classes.Resource.ResourceType getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(classes.Resource.ResourceType resourceType) {
+        this.resourceType = resourceType;
+    }
+
+}
