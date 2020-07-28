e98d0faf4c01aa46a53c03ac093cf2ca5d20a7eb
aki <kieran.mortimer@gmail.com>
2020-07-28 10:33:30 +0200

    Zoo manager and employee WIP classes

--- src/classes/ZooManager.java
+++ src/classes/ZooManager.java
@@ -1,4 +1,30 @@
 package classes;
 
+
+import java.util.HashMap;
+
 public class ZooManager {
+    private HashMap<Integer, ZooEmployee> employeeHashMap;
+
+    public ZooManager() {
+        employeeHashMap = new HashMap<>();
+    }
+
+    public void hireEmployee(ZooEmployee zooEmployee) {
+        employeeHashMap.put(zooEmployee.getEmployeeID(), zooEmployee);
+        zooEmployee.setActiveEmployee(true);
+    }
+
+    public void fireEmployee(ZooEmployee zooEmployee) {
+        zooEmployee.setActiveEmployee(false);
+        employeeHashMap.remove(zooEmployee.getEmployeeID());
+    }
+
+    // TODO - Purchase Animals
+    // TODO - Get animal's health
+    // TODO - Print to file a report
+
+    public HashMap<Integer, ZooEmployee> getEmployeeHashMap() {
+        return employeeHashMap;
+    }
 }
